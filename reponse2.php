7feba01f0f38e43b9ca4028e2a440631b49ac04f
sarah klewiec <sarahklewiec@gmail.com>
2018-02-14 13:00:51 +0100

    version1402

--- reponse2.php
+++ reponse2.php
@@ -10,13 +10,13 @@
       <img src="logo.png" alt="logo Hackers Poulette"/>
     </section>
     <?php
-      $genre=$_POST["gender"];
-      $nom=$_POST["lastname"];
-      $prenom=$_POST["firstname"];
-      $pays=$_POST["country"];
+      $genre=$_POST["genre"];
+      $nom=$_POST["nom"];
+      $prenom=$_POST["prenom"];
+      $pays=$_POST["pays"];
       $email=$_POST["email"];
-      $sujet=$_POST["subject"];
-      $message=$_POST["mainmessage"];
+      $sujet=$_POST["sujet"];
+      $message=$_POST["message"];
      ?>
 
      <p class="merci"> L'équipe technique de <strong>Hackers Poulette</strong> vous remercie pour votre message <?php echo $genre $nom ?>.<br/>
@@ -24,10 +24,10 @@
      <h2> Vos informations: </h2>
      <div class="confirmation">
      Nom, prénom: <?php echo $nom, $prenom;?> <br/>
-     Pays: <?php echo "$pays"; ?> <br/>
-     Email: <?php echo "$email"; ?> <br/>
-     Sujet de votre demande: <?php echo "$sujet"; ?> <br/>
-     Votre message: <?php echo "$message"; ?> <br/>
+     Pays: <?php echo $pays; ?> <br/>
+     Email: <?php echo $email; ?> <br/>
+     Sujet de votre demande: <?php echo $sujet; ?> <br/>
+     Votre message: <?php echo $message; ?> <br/>
    </div>
     <form action="contact.php" method="post">
       <div id="retour">
